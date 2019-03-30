90933a3e618adc0042669c5bb4f7676d65746fe0
aadityamaddukuri <46496817+aadityamaddukuri@users.noreply.github.com>
2019-03-30 15:49:22 -0500

    Add files via upload

---
+++ okay.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import lxml
+import urllib.request
+
+def main():
+    url=urllib.request.urlopen("https://www.google.com/")
+    data=url.read()
+    soup=BeautifulSoup(data,"html.parser")
+    print(soup.prettify())
+
+main()
+
+
