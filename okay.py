eb611cd9eeb865daca31f64778735e57d7cf4711
aadityamaddukuri <46496817+aadityamaddukuri@users.noreply.github.com>
2019-04-02 12:01:14 -0500

    Add files via upload

--- okay.py
+++ okay.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 import lxml
 import urllib.request
 
+
+
 def main():
-    url=urllib.request.urlopen("https://www.google.com/")
+    url=urllib.request.urlopen("http://www.ms2soft.com/tcds/?loc=Minneapolis&mod=tcds&local_id=84")
     data=url.read()
     soup=BeautifulSoup(data,"html.parser")
-    print(soup.find_all('td'))
+    print(soup.find_all('tbody'))
+   
 
 main()
 
